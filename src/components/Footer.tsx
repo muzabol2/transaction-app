17194b6fd3642954b67d339a2b70cc2e632403f4
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-11-29 12:07:12 +0100

    Navbar and Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+export default function Footer() {
+   return (
+      <footer className="footer">
+         <ul>
+            <li>Ochrona danych</li>
+            <li>Zastrzeżenia prawne</li>
+            <li>BIC (Swift): BIGBPLPWXXX</li>
+            <li>{"Copyright © BM ;)"}</li>
+         </ul>
+      </footer>
+   );
+}
